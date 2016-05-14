2a65123b74e78800138529fb0ef2d68fc4a2830e
Josh Leverette <coder543@gmail.com>
2016-05-13 23:32:03 -0500

    Removed needless 'loop', brought code into conformity with cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use sim_data::Star;
 
 use stopwatch::Stopwatch;
 
-use std::io::{stdin,stdout,BufRead,Write};
+use std::io::{stdin, stdout, BufRead, Write};
 
 fn load_database() -> Result<Vec<Star>, &'static str> {
 
@@ -29,9 +29,10 @@ fn load_database() -> Result<Vec<Star>, &'static str> {
     println!("Initializing database from csv...");
 
     let stars_wrap: csv::Result<Vec<Star>> = reader.decode().collect::<csv::Result<Vec<Star>>>();
-    
+
     if let Ok(stars) = stars_wrap {
-        println!("Loading took {} seconds\n", sw.elapsed_ms() as f32 / 1000f32);
+        println!("Loading took {} seconds\n",
+                 sw.elapsed_ms() as f32 / 1000f32);
         return Ok(stars);
     } else {
         return Err("Unable to decode csv file!");
@@ -42,51 +43,49 @@ fn main() {
     let maxdist: f64;
     let stars = load_database().unwrap();
     let stdin = stdin();
-    // loop {
-        let stars = stars.clone();
-        loop {
-            print!("> ");
-            stdout().flush().unwrap();
-            let mut line = String::new();
-            stdin.lock().read_line(&mut line).unwrap();
-            let line = line.trim();
-            if line == "q" {
-                return;
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+        let mut line = String::new();
+        stdin.lock().read_line(&mut line).unwrap();
+        let line = line.trim();
+        if line == "q" {
+            return;
+        } else {
+            let maxdist_wrap = line.parse::<f64>();
+            if let Ok(dist) = maxdist_wrap {
+                maxdist = dist * 0.306601;
+                break;
             } else {
-                let maxdist_wrap = line.parse::<f64>();
-                if let Ok(dist) = maxdist_wrap {
-                    maxdist = dist * 0.306601;
-                    break;
-                } else { 
-                    println!("Please enter either 'q' to quit, or a distance in lightyears.");
-                }
+                println!("Please enter either 'q' to quit, or a distance in lightyears.");
             }
         }
-        let sw = Stopwatch::start_new();
-        let mut named_vec = Vec::new();
-        for star in stars {
-            //let Some(mscatid) = star.multistar_catalog_id.clone()
-            if star.distance < maxdist {
-                named_vec.push(star.clone());
-            }
+    }
+    let sw = Stopwatch::start_new();
+    let mut named_vec = Vec::new();
+    for star in stars {
+        // let Some(mscatid) = star.multistar_catalog_id.clone()
+        if star.distance < maxdist {
+            named_vec.push(star.clone());
         }
-        named_vec.sort_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap());
-        let starcount = named_vec.len();
-        let mut window = Window::new(&format!("Stars within {:.3} lightyears", maxdist * 3.26156));
-        window.set_light(Light::StickToCamera);
-        for star in named_vec {
-            let mut sph = window.add_sphere(0.01);
-            if star.id == 0 {
-                sph.set_color(1.0, 1.0, 0.7);
-            } else {
-                sph.set_color(0.7, 0.7, 1.0);
-            }
-            sph.append_translation(&Vector3::new(star.x as f32, star.y as f32, star.z as f32));
-            println!("{:.3} lightyears\n{:?}\n\n", star.distance * 3.26156, star);
+    }
+    named_vec.sort_by(|a, b| a.distance.partial_cmp(&b.distance).unwrap());
+    let starcount = named_vec.len();
+    let mut window = Window::new(&format!("Stars within {:.3} lightyears", maxdist * 3.26156));
+    window.set_light(Light::StickToCamera);
+    for star in named_vec {
+        let mut sph = window.add_sphere(0.01);
+        if star.id == 0 {
+            sph.set_color(1.0, 1.0, 0.7);
+        } else {
+            sph.set_color(0.7, 0.7, 1.0);
         }
-        println!("There were {} matching stars.", starcount);
-        println!("Processing took {} seconds", sw.elapsed_ms() as f32 / 1000f32);
-        while window.render() {}
-        window.hide();
-    // }
-}   
+        sph.append_translation(&Vector3::new(star.x as f32, star.y as f32, star.z as f32));
+        println!("{:.3} lightyears\n{:?}\n\n", star.distance * 3.26156, star);
+    }
+    println!("There were {} matching stars.", starcount);
+    println!("Processing took {} seconds",
+             sw.elapsed_ms() as f32 / 1000f32);
+    while window.render() {}
+    window.hide();
+}
--- src/sim_data.rs
+++ src/sim_data.rs
@@ -1,7 +1,6 @@
 #[derive(RustcEncodable, RustcDecodable, Debug, Clone)]
-pub struct Star
-{
-    //various means of ID
+pub struct Star {
+    // various means of ID
     pub id: u32,
     pub hipparcos: Option<u32>,
     pub henry_draper: Option<u32>,
@@ -9,40 +8,40 @@ pub struct Star
     pub gliese: Option<u32>,
     pub bayer_flamsteed: Option<String>,
     pub common_name: Option<String>,
-    
-    //physical properties
+
+    // physical properties
     pub ra: f64,
     pub dec: f64,
-    pub distance: f64, //unit is parsecs
-    pub pmra: f64, //proper motion, right ascension  milliarcseconds per year
-    pub pmdec: f64,//proper motion, declination,     milliarcseconds per year
+    pub distance: f64, // unit is parsecs
+    pub pmra: f64, // proper motion, right ascension  milliarcseconds per year
+    pub pmdec: f64, // proper motion, declination,     milliarcseconds per year
     pub radial_velocity: Option<f64>,
-    pub magnitude: f64, //apparent visual magnitude
-    pub abs_magnitude: f64, //apparent mag normalized to 10 parsecs
+    pub magnitude: f64, // apparent visual magnitude
+    pub abs_magnitude: f64, // apparent mag normalized to 10 parsecs
     pub spectral_type: Option<String>,
     pub color_index: Option<f64>,
-    pub x: f64, //unit is parsecs
-    pub y: f64, //unit is parsecs
-    pub z: f64, //unit is parsecs
-    pub vx: f64, //velocity on x axis, unit is parsecs per year
-    pub vy: f64, //velocity on y axis, unit is parsecs per year
-    pub vz: f64, //velocity on z axis, unit is parsecs per year
-    pub rarad: f64,  //ra in radians
-    pub decrad: f64, //dec in radians
-    pub pmrarad: f64, //proper motion in radians per year
-    pub pmdecrad: f64, //proper motion in radians per year
+    pub x: f64, // unit is parsecs
+    pub y: f64, // unit is parsecs
+    pub z: f64, // unit is parsecs
+    pub vx: f64, // velocity on x axis, unit is parsecs per year
+    pub vy: f64, // velocity on y axis, unit is parsecs per year
+    pub vz: f64, // velocity on z axis, unit is parsecs per year
+    pub rarad: f64, // ra in radians
+    pub decrad: f64, // dec in radians
+    pub pmrarad: f64, // proper motion in radians per year
+    pub pmdecrad: f64, // proper motion in radians per year
     pub bayer: Option<String>,
     pub flam: Option<u32>,
     pub constellation: Option<String>,
-    
-    //Gliese multi-star system information
+
+    // Gliese multi-star system information
     pub companion_id: i32,
     pub primary_star_id: i32,
     pub multistar_catalog_id: Option<String>,
-    
-    //misc physical properties
-    pub lum: f64, //luminosity as a multiple of solar luminosity
-    pub var: Option<String>,  //variable star designation
-    pub var_min: Option<f64>, //approximate minimum magnitude for variable star
-    pub var_max: Option<f64>  //approximate maximum magnitude for variable star
+
+    // misc physical properties
+    pub lum: f64, // luminosity as a multiple of solar luminosity
+    pub var: Option<String>, // variable star designation
+    pub var_min: Option<f64>, // approximate minimum magnitude for variable star
+    pub var_max: Option<f64>, // approximate maximum magnitude for variable star
 }
