2fdc3d33d800c0c4663a841d04f81c04f9491210
Josh Leverette <coder543@gmail.com>
2016-09-22 10:17:52 -0500

    Make code a bit more idiomatic

--- src/sim_data.rs
+++ src/sim_data.rs
@@ -45,3 +45,64 @@ pub struct Star {
     pub var_min: Option<f64>, // approximate minimum magnitude for variable star
     pub var_max: Option<f64>, // approximate maximum magnitude for variable star
 }
+
+
+impl Star {
+    // translated from http://stackoverflow.com/a/22630970/389837
+    pub fn bv2rgb_opt(&self) -> Option<(f32, f32, f32)> {
+        self.color_index.map(|_| self.bv2rgb())
+    }
+
+    pub fn bv2rgb(&self) -> (f32, f32, f32) {
+        let mut bv = self.color_index.unwrap();
+        let r: f64;
+        let g: f64;
+        let b: f64;
+        let mut t: f64;
+        if bv < -0.4 {
+            bv = -0.4;
+        }
+        if bv > 2.0 {
+            bv = 2.0;
+        }
+        if (bv >= -0.40) && (bv < 0.00) {
+            t = (bv + 0.40) / (0.00 + 0.40);
+            r = 0.61 + (0.11 * t) + (0.1 * t * t);
+        } else if (bv >= 0.00) && (bv < 0.40) {
+            t = (bv - 0.00) / (0.40 - 0.00);
+            r = 0.83 + (0.17 * t);
+        } else if (bv >= 0.40) && (bv < 2.10) {
+            r = 1.00;
+        } else {
+            r = 1.0;
+        }
+        if (bv >= -0.40) && (bv < 0.00) {
+            t = (bv + 0.40) / (0.00 + 0.40);
+            g = 0.70 + (0.07 * t) + (0.1 * t * t);
+        } else if (bv >= 0.00) && (bv < 0.40) {
+            t = (bv - 0.00) / (0.40 - 0.00);
+            g = 0.87 + (0.11 * t);
+        } else if (bv >= 0.40) && (bv < 1.60) {
+            t = (bv - 0.40) / (1.60 - 0.40);
+            g = 0.98 - (0.16 * t);
+        } else if (bv >= 1.60) && (bv < 2.00) {
+            t = (bv - 1.60) / (2.00 - 1.60);
+            g = 0.82 - (0.5 * t * t);
+        } else {
+            g = 1.0;
+        }
+        if (bv >= -0.40) && (bv < 0.40) {
+            b = 1.00;
+        } else if (bv >= 0.40) && (bv < 1.50) {
+            t = (bv - 0.40) / (1.50 - 0.40);
+            b = 1.00 - (0.47 * t) + (0.1 * t * t);
+        } else if (bv >= 1.50) && (bv < 1.94) {
+            t = (bv - 1.50) / (1.94 - 1.50);
+            b = 0.63 - (0.6 * t * t);
+        } else {
+            b = 1.0;
+        }
+
+        (r as f32, g as f32, b as f32)
+    }
+}
